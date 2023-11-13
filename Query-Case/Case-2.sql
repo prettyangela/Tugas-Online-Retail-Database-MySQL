cca2a4044b36a2180cf35df6e8585b3441afd918
prettyangela <angelala6600@gmail.com>
2023-11-13 09:14:17 +0700

    update case 2 add daftar nama produk

--- Query-Case/Case-2.sql
+++ Query-Case/Case-2.sql
@@ -12,3 +12,19 @@ LIMIT 3;
 |          8 |               5 |
 |         13 |               4 |
 +------------+-----------------+
+
+-- Daftar nama produk yang sering di order
+SELECT Products.product_name, COUNT(OrderDetail.product_id) as total_orders
+FROM OrderDetail
+JOIN Products ON OrderDetail.product_id = Products.product_id
+GROUP BY Products.product_name
+ORDER BY total_orders DESC
+LIMIT 3;
+
++--------------+--------------+
+| product_name | total_orders |
++--------------+--------------+
+| Dress        |            5 |
+| Tango Waffer |            5 |
+| Smartphone   |            4 |
++--------------+--------------+
